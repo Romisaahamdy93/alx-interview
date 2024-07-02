7ba455b5703165f49f67b085f3fdb01ee1aafeee
Romisaa <romisaa.hamdy@yahoo.com>
2024-07-02 23:09:29 +0000

    task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script will unlock list of lists"""
+
+def canUnlockAll(boxes):
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
+    """
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    return len(keys) == len(boxes)
+
+# Test cases
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))  # False
